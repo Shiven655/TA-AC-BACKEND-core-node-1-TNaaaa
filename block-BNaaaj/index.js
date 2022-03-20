e4d0767508503cd2a15bec3861be65714c869597
Shiven655 <kshiven655@gmail.com>
2022-03-20 11:51:09 +0530

    block-BJaaaJ

--- block-BNaaaj/index.js
+++ block-BNaaaj/index.js
@@ -7,5 +7,15 @@ let uptime = os.uptime();
 console.log(cpus, freemem, uptime);
 
 var buff1 = Buffer.alloc(12);
+var buff2 = Buffer.allocUnsafe(12);
+
 buff1.write('Shivender Kumar');
 console.log(buff1.toString());
+
+var { readFile, readFileSync, unlink } = require('fs');
+
+var sync = readFileSync('./app.js');
+
+readFile('./app.js', (err, content) => {
+  console.log(err, content.toString);
+});
